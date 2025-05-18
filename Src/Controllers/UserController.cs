c049ca7c506693a1e7205608675ebbdf10191ab3
danukaz <--list>
2025-05-18 19:19:02 -0400

    fix(): Fixed ToggleStatus endpoint to correctly update user IsActive status

--- Src/Controllers/UserController.cs
+++ Src/Controllers/UserController.cs
@@ -89,7 +89,7 @@ namespace Taller.Src.Controllers
 
             user.IsActive = dto.IsActive;
             user.DeactivationReason = dto.IsActive ? null : dto.Reason;
-            
+
             await _unitOfWork.UserRepository.UpdateUserAsync(user);
             await _unitOfWork.SaveChangeAsync();
 
